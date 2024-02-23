27fc14237b0ee9e691eb3a2d59a9f73d675308c4
Aamer <aamer@192-168-1-120.tpgi.com.au>
2024-02-23 11:02:27 +1100

    a dummy Docker image can be built and pushed to ECR using CDK

--- cdk_workspace/cdk_workspace_stack.py
+++ cdk_workspace/cdk_workspace_stack.py
@@ -3,17 +3,30 @@ from aws_cdk import (
     Stack,
     # aws_sqs as sqs,
 )
+import aws_cdk.aws_ecr_assets as ecr_assets
 from constructs import Construct
+import aws_cdk.aws_ecr as ecr
+import os
+
 
 class CdkWorkspaceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create an ECR repository
+        repository = ecr.Repository(
+            self,
+            "MyRepository",
+            repository_name="my-repo-name",
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkWorkspaceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Build and upload Docker image to ECR
+        asset = ecr_assets.DockerImageAsset(
+            self,
+            "MyDockerImage",
+            directory=os.path.join(
+                os.path.dirname(__file__), ".."
+            ),  # Adjust the path as necessary
+            file="Dockerfile",  # Specify the Dockerfile name
+        )
